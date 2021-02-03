67ac9fb363636a10ff89b308033166f6c6743b75
Pratham Pujari <71821987+Pratham340@users.noreply.github.com>
2021-02-03 21:20:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+song = "";
+objects = [];
+status = "";
+
+function preload()
+{
+	song = loadSound("alert.mp3");
+}
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  canvas.position(580, 250);
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+ 
+          fill(color(r,g,b));
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(color(r,g,b));
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+          if(objects[i].label == "person")
+          {
+            document.getElementById("objects").innerHTML = "Baby Found";
+            console.log("stop");
+            song.stop();
+          }
+          else
+          {
+            document.getElementById("objects").innerHTML = "Baby Not Found";
+            console.log("play"); 
+            song.play();
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          document.getElementById("objects").innerHTML = "Baby Not Found";
+          console.log("play"); 
+          song.play();
+        }
+      }
+}
+
+email1 = localStorage.getItem("email1");
+  document.getElementById("user_name").innerHTML= "<h1>Welcome "+email1+"!</h1>";
